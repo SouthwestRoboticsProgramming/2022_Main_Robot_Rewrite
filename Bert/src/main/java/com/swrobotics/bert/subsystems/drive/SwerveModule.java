1b318f268c51bf3020bd3a4ff36750f28f6f048e
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-03-07 22:06:56 -0600

    CANCoder as feedback for SRX

--- Bert/src/main/java/com/swrobotics/bert/subsystems/drive/SwerveModule.java
+++ Bert/src/main/java/com/swrobotics/bert/subsystems/drive/SwerveModule.java
@@ -1,9 +1,17 @@
 package com.swrobotics.bert.subsystems.drive;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.RemoteFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.swrobotics.bert.subsystems.Subsystem;
+
+import org.ejml.ops.ReadCsv;
+
+import edu.wpi.first.wpilibj.Encoder;
 
 import static com.swrobotics.bert.constants.Constants.*;
 
@@ -19,6 +27,11 @@ public class SwerveModule implements Subsystem {
     drive = new TalonFX(driveID, CANIVORE);
     turn = new TalonSRX(turnID);
     can = new CANCoder(canCoderID);
+
+    TalonSRXConfiguration turnConfig = new TalonSRXConfiguration();
+    turnConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor0;
+    turnConfig.remoteFilter0.remoteSensorDeviceID = canCoderID;
+    turnConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.CANCoder;
 
     can.setPosition(canCoderOffset);
   }
