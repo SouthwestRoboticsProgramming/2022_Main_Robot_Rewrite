07f5a58d853cbcdc9837345f6e2902540bcb9502
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-03-18 18:50:45 -0500

    Next steps

--- Bert/src/main/java/com/swrobotics/bert/commands/climber/ClimberSequence.java
+++ Bert/src/main/java/com/swrobotics/bert/commands/climber/ClimberSequence.java
@@ -9,6 +9,8 @@ public final class ClimberSequence extends CommandSequence {
     public ClimberSequence(Climber climber) {
         // append(new ClimberStep(tele, rot, loaded));
         append(new ClimberStep(climber, CLIMB_STEP_1_TELE.get(), CLIMB_STEP_1_ROT.get(), false));
+        append(new ClimberStep(climber, CLIMB_STEP_2_TELE.get(), CLIMB_STEP_1_ROT.get(), true));
+        append(new ClimberStep(climber, CLIMB_STEP_2_TELE.get(), CLIMB_STEP_3_ROT.get(), true));
     }
 
     @Override
