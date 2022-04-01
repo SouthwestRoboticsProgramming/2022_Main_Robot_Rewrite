e99c5b69ecd3fce1fbc6003713b485248abdf682
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-04-01 11:59:52 -0500

    Add ball position diagram

--- Bert/src/main/java/com/swrobotics/bert/constants/ball/BallLocationConstants.java
+++ Bert/src/main/java/com/swrobotics/bert/constants/ball/BallLocationConstants.java
@@ -2,6 +2,20 @@ package com.swrobotics.bert.constants.ball;
 
 public final class BallLocationConstants {
 
+    /*
+     *
+     * +---------------------------------------+
+     * |    |           \ B6 R6             R7\|
+     * |----+         R1 \     R5              |
+     * |             B1   \     B5             |
+     * |                   .                   |
+     * |             R2     \   R4             |
+     * |              B2     \ B4         +----|
+     * |\B7             B3 R3 \           |    |
+     * +---------------------------------------+ 
+     *              [Scoring table]
+     */
+
     public static final BallLocation BLUE_1 = new BallLocation(0, 0);
     public static final BallLocation BLUE_2 = new BallLocation(0, 0);
     public static final BallLocation BLUE_3 = new BallLocation(0, 0);
