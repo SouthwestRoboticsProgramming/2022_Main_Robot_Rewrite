04d67bacb76ddc0ce311b3ec95b996bf3816e1f0
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-04-01 11:50:02 -0500

    Space

--- Bert/src/main/java/com/swrobotics/bert/constants/ball/BallLocationConstants.java
+++ Bert/src/main/java/com/swrobotics/bert/constants/ball/BallLocationConstants.java
@@ -9,7 +9,7 @@ public final class BallLocationConstants {
     public static final BallLocation BLUE_5 = new BallLocation(0, 0);
     public static final BallLocation BLUE_6 = new BallLocation(0, 0);
     public static final BallLocation BLUE_7 = new BallLocation(0, 0);
-    public static final BallLocation RED_1= new BallLocation(0, 0);
+    public static final BallLocation RED_1 = new BallLocation(0, 0);
     public static final BallLocation RED_2 = new BallLocation(0, 0);
     public static final BallLocation RED_3 = new BallLocation(0, 0);
     public static final BallLocation RED_4 = new BallLocation(0, 0);
