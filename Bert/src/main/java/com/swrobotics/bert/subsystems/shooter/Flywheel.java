b6141f3629eb2174e2fd45ee7dd1b3663b3e4d74
mvog2501 <63077980+rmheuer@users.noreply.github.com>
2022-04-01 12:41:10 -0500

    Ball locator

--- Bert/src/main/java/com/swrobotics/bert/subsystems/shooter/Flywheel.java
+++ Bert/src/main/java/com/swrobotics/bert/subsystems/shooter/Flywheel.java
@@ -43,13 +43,13 @@ public final class Flywheel implements Subsystem {
     public void robotPeriodic() {
         // flywheel.set(TalonFXControlMode.PercentOutput, 1);
         // System.out.println(flywheel.getTemperature());
+        // System.out.println(flywheel.getIntegralAccumulator());
     }
 
     public void setFlywheelSpeed(double rpm) {
-        System.out.println("Flywheel: " + rpm);
+        // System.out.println("Flywheel: " + rpm + " Actual: " + flywheel.getSelectedSensorVelocity() / RPM_TO_FX_VELOCITY / FLYWHEEL_GEAR_RATIO);
         flywheel.set(TalonFXControlMode.Velocity, rpm * RPM_TO_FX_VELOCITY * FLYWHEEL_GEAR_RATIO);
 //    System.out.println("Current: " + flywheel.getSelectedSensorVelocity() /
 //    RPM_TO_FX_VELOCITY / FLYWHEEL_GEAR_RATIO + " Target: " + rpm);
     }
-
 }
