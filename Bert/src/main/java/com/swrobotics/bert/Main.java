acb19ca42c7d4b7082233e75e9b56307efd4777e
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-04-11 17:24:43 -0500

    Bert: Remove NetworkTables reset in Main

--- Bert/src/main/java/com/swrobotics/bert/Main.java
+++ Bert/src/main/java/com/swrobotics/bert/Main.java
@@ -5,11 +5,6 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Main {
     public static void main(String[] args) {
         RobotBase.startRobot(Robot::get);
-
-        // File file = new File("/home/lvuser/networktables.ini");
-        // file.delete();
-
-        // System.out.println("Deleted it");
     }
 
     private Main() {
