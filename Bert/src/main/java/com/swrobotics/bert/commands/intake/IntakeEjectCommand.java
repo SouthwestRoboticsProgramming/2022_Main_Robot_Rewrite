05b8a58410cec8e633f1284a6ae761c4e9fee08b
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-04-15 13:06:47 -0500

    Rewrite IntakeEjectCommand as a sequence

--- Bert/src/main/java/com/swrobotics/bert/commands/intake/IntakeEjectCommand.java
+++ Bert/src/main/java/com/swrobotics/bert/commands/intake/IntakeEjectCommand.java
@@ -1,47 +1,23 @@
 package com.swrobotics.bert.commands.intake;
 
-import com.swrobotics.bert.commands.Command;
+import com.swrobotics.bert.commands.CommandSequence;
+import com.swrobotics.bert.commands.shooter.IndexCommand;
 import com.swrobotics.bert.subsystems.intake.Intake;
+import com.swrobotics.bert.subsystems.intake.Intake.State;
 import com.swrobotics.bert.subsystems.shooter.Hopper;
 
-import static com.swrobotics.bert.constants.Constants.PERIODIC_PER_SECOND;
 import static com.swrobotics.bert.constants.IntakeConstants.*;
 import static com.swrobotics.bert.constants.ShooterConstants.*;
 
-// FIXME-TODO: Wait before indexing
-public final class IntakeEjectCommand implements Command {
-    private final Intake intake;
-    private final Hopper hopper;
-    private final Intake.State savedState;
-    private int timer;
-
+public final class IntakeEjectCommand extends CommandSequence {
     public IntakeEjectCommand(Intake intake, Hopper hopper) {
-        this.intake = intake;
-        Intake.State state = intake.getState();
-        if (state == Intake.State.EJECT) {
-            state = Intake.State.OFF;
+        State savedState = intake.getState();
+        if (savedState == State.EJECT) {
+            savedState = State.OFF;
         }
-        savedState = state;
-        this.hopper = hopper;
-        this.timer = (int) (EJECT_TIME.get() * PERIODIC_PER_SECOND);
-    }
 
-    @Override
-    public void init() {
-
-    }
-
-    @Override
-    public boolean run() {
-        intake.setState(Intake.State.EJECT);
-        hopper.setIndexSpeed(INDEX_EJECT_SPEED.get());
-
-        return --timer <= 0;
-    }
-
-    @Override
-    public void end() {
-        intake.setState(savedState);
-        hopper.setIndexSpeed(0);
+        append(new IntakeSetCommand(intake, State.EJECT));
+        append(new IndexCommand(hopper, INDEX_EJECT_SPEED.get(), EJECT_TIME.get()));
+        append(new IntakeSetCommand(intake, savedState));
     }
 }
